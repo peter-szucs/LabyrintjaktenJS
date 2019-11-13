53934dd197657957433f6b0e2c2e0cffc1ec6a9a
peter-szucs <55179291+peter-szucs@users.noreply.github.com>
2019-11-13 13:43:40 +0100

    external mapinfo file
    making external map info file for mapArray and monsterpaths

---
+++ map1.js
@@ -0,0 +1,32 @@
+const mapArray = [
+    [5, 1, 9, 1, 1, 1, 1, 1, 1, 1, 6],
+    [3, 0, 14, 0, 0, 0, 0, 0, 0, 0, 4],
+    [3, 0, 14, 0, 16, 0, 26, 13, 13, 13, 12],
+    [3, 0, 15, 0, 14, 0, 14, 0, 0, 0, 4],
+    [3, 0, 0, 0, 14, 0, 15, 0, 18, 13, 12],
+    [11, 13, 13, 13, 24, 0, 0, 0, 0, 0, 4],
+    [3, 0, 0, 0, 0, 0, 26, 13, 25, 0, 4],
+    [3, 0, 26, 13, 13, 13, 24, 0, 14, 0, 4],
+    [3, 0, 15, 0, 0, 0, 0, 0, 23, 13, 12],
+    [3, 0, 0, 0, 16, 0, 16, 0, 0, 0, 4],
+    [11, 13, 13, 13, 21, 0, 23, 13, 13, 13, 12],
+    [3, 0, 0, 0, 14, 0, 0, 0, 0, 0, 0],
+    [3, 0, 16, 0, 14, 0, 18, 13, 25, 0, 4],
+    [3, 0, 14, 0, 15, 0, 0, 0, 14, 0, 4],
+    [3, 0, 14, 0, 0, 0, 16, 0, 14, 0, 4],
+    [3, 0, 23, 13, 13, 13, 20, 13, 24, 0, 4],
+    [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7]
+];
+const gameTiles = 32;
+const monster1StartX = 5 * gameTiles;
+const monster1EndX = (4 * gameTiles) + monster1StartX;
+const monster1StartY = (5 * gameTiles) - 12;
+const monster2StartX = 3 * gameTiles;
+const monster2MidX = 7 * gameTiles;
+const monster2EndX = 7 * gameTiles;
+const monster2StartY = (8 * gameTiles) - 12;
+const monster2EndY = (9 * gameTiles) - 12;
+const monster3StartX = 5 * gameTiles;
+const monster3EndX = 9 * gameTiles;
+const monster3StartY = (11 * gameTiles) - 12;
